06b2067da66d81136d92abf82bb6d599c7428098
Satya Rohith <me@satyarohith.com>
2021-01-11 10:52:52 +0530

    use secrets

--- tom.ts
+++ tom.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/x/sift@0.1.1/mod.js";
 
 serve({
-  "/": () => new Response("Hello from Tom! :)")
+  "/": () => new Response(`SECRET: ${Deno.env.get("SECRET")}`)
 });

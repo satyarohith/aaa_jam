940a91b0f3a5471c58e6f5435d29663e773f61d6
Satya Rohith <me@satyarohith.com>
2025-04-27 12:33:14 +0530

    Update tom.ts

--- tom.ts
+++ tom.ts
@@ -1,5 +1,1 @@
-import { serve } from "https://deno.land/x/sift@0.1.1/mod.js";
-
-serve({
-  "/": () => new Response(`SECRET: ${Deno.env.get("SECRET")}`)
-});
+Deno.serve(req => new Response("hello world"));
